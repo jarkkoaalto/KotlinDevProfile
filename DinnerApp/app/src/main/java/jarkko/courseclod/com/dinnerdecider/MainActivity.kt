7aa81cb588410032d7b07c5e024a7076ff6cb148
Jarkko Aalto <jarkko.aalto@live.fi>
2019-04-19 13:52:48 +0300

    App functional testing

--- DinnerApp/app/src/main/java/jarkko/courseclod/com/dinnerdecider/MainActivity.kt
+++ DinnerApp/app/src/main/java/jarkko/courseclod/com/dinnerdecider/MainActivity.kt
@@ -3,13 +3,31 @@ package jarkko.courseclod.com.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    public val foodList = arrayListOf<String>("Chinese","Hamburger","Pizza","Carols","Hesburger","SubWay")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodText.text = "Hesburger"
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList(randomFood)
+        }
+
+        addFoodBtn.setOnClickListener{
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
     }
 }
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KotlinDevProfile
 BitDegree Course. Kotlin Tutorial for a Beginner: Learn How to make an Android App
+
+
+App purpose is indroduction to the Kotlin programming. You can add you favorite woods.
+
+[logo]: /images/icon48.png "DinnerApp main view"
